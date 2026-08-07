53dcba57d83f12e1c47102e650ccc22ef4b2f6a6
unknown <smtpnotification0@gmail.com>
2026-08-07 12:47:35 +0600

    auto: update 2026-08-07 12:47:35

--- apps/user-frontend/layouts/default.vue
+++ apps/user-frontend/layouts/default.vue
@@ -446,7 +446,7 @@ watch([pgwAppEnabled, pgwForceInstallEnabled, pwaInstalledPersisted], () => {
 
         <!-- COPYRIGHT -->
         <div class="border-t border-white/10 pt-6 text-center text-[12px] text-slate-400">
-          ©  2025 | All Rights Reserved | Developed by <span class="font-bold text-white"></span>
+          ©  2026 | All Rights Reserved | Developed by <span class="font-bold text-white"></span>
         </div>
 
       </div>
